b7bc6fc46775a6b67e0346be0851b1585111e0a2
wasimahmedqureshi <wasimahmedqureshi@gmail.com>
2026-03-11 15:40:06 +0530

    Add count variable and increase/reset functions

--- tasbeeh.js
+++ tasbeeh.js
@@ -1,1 +1,17 @@
+var count = 0;
 
+function increase(){
+
+count++;
+
+document.getElementById("count").innerText = count;
+
+}
+
+function reset(){
+
+count = 0;
+
+document.getElementById("count").innerText = count;
+
+}
