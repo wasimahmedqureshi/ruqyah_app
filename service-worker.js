533e1f04af5b9888925788c8d40c67eb476edb0a
wasimahmedqureshi <wasimahmedqureshi@gmail.com>
2026-03-16 11:59:12 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,34 @@
-var cacheName="ruqyah-cache"
+const cacheName = 'ruqyah-cache-v2';
 
-self.addEventListener("install",function(e){
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/manifest.json',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
+      ]);
+    })
+  );
+});
 
-e.waitUntil(
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
 
-caches.open(cacheName).then(function(cache){
-
-return cache.addAll([
-
-"/",
-"/index.html",
-"/style.css",
-"/script.js"
-
-])
-
-})
-
-)
-
-})
-
-self.addEventListener("fetch",function(e){
-
-e.respondWith(
-
-caches.match(e.request).then(function(response){
-
-return response || fetch(e.request)
-
-})
-
-)
-
-})
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+      );
+    })
+  );
+});
